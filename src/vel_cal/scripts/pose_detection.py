8a70846f440ace12dd586cfaddbd7ef50a8ad040
stimson06 <stimsonpushparaj@gmail.com>
2021-08-06 14:45:33 +0530

    Human follow

--- src/vel_cal/scripts/pose_detection.py
+++ src/vel_cal/scripts/pose_detection.py
@@ -13,37 +13,37 @@ class pose_detection():
         self.Known_width = 44.0 # sholder_width of the person 
         
     def coordinates(self, results, h, w):
-    """
-    Params:
-    @ results: contains all the detected cooridates (32 points)
-    @ h : height of the frame
-    @ w : width of the frame
-    
-    Return:
-     (x,y) coordinates of hip and shoulder (Right & Left ) """
+        """
+        Params:
+        @ results: contains all the detected cooridates (32 points)
+        @ h : height of the frame
+        @ w : width of the frame
         
-            landmarks = results.pose_landmarks.landmark
-            Rshoulder = landmarks[self.mp_pose.PoseLandmark.RIGHT_SHOULDER]
-            Lshoulder = landmarks[self.mp_pose.PoseLandmark.LEFT_SHOULDER] 
-            Rhip = landmarks[self.mp_pose.PoseLandmark.RIGHT_HIP] 
-            Lhip = landmarks[self.mp_pose.PoseLandmark.LEFT_HIP] 
-            RSHcoordinate = (int(Rshoulder.x*w), int(Rshoulder.y*h))
-            LSHcoordinate = (int(Lshoulder.x*w), int(Lshoulder.y*h))
-            RHcoordinate =  (int(Rhip.x*w), int(Rhip.y*h))
-            LHcoordinate =  (int(Lhip.x*w), int(Lhip.y*h))
-            
-            return RSHcoordinate, LSHcoordinate, RHcoordinate, LHcoordinate
+        Return:
+        (x,y) coordinates of hip and shoulder (Right & Left ) """
+        
+        landmarks = results.pose_landmarks.landmark
+        Rshoulder = landmarks[self.mp_pose.PoseLandmark.RIGHT_SHOULDER]
+        Lshoulder = landmarks[self.mp_pose.PoseLandmark.LEFT_SHOULDER] 
+        Rhip = landmarks[self.mp_pose.PoseLandmark.RIGHT_HIP] 
+        Lhip = landmarks[self.mp_pose.PoseLandmark.LEFT_HIP] 
+        RSHcoordinate = (int(Rshoulder.x*w), int(Rshoulder.y*h))
+        LSHcoordinate = (int(Lshoulder.x*w), int(Lshoulder.y*h))
+        RHcoordinate =  (int(Rhip.x*w), int(Rhip.y*h))
+        LHcoordinate =  (int(Lhip.x*w), int(Lhip.y*h))
+
+        return RSHcoordinate, LSHcoordinate, RHcoordinate, LHcoordinate
 
     def line_intersect(self,Ax1, Ay1, Ax2, Ay2, Bx1, By1, Bx2, By2):
-    """ 
-    Params:
-    @ Ax1, Ay1 : Coordinates of Left Shoulder
-    @ Ax2, Ay2 : Coordinates of Right Hip
-    @ Bx1, By1 : Coordinates of Right Shoulder
-    @ Bx2, By2 : Coordinates of Left Hip
-    
-    Returns:
-    (x, y) tuple or None if there is no intersection """
+        """ 
+        Params:
+        @ Ax1, Ay1 : Coordinates of Left Shoulder
+        @ Ax2, Ay2 : Coordinates of Right Hip
+        @ Bx1, By1 : Coordinates of Right Shoulder
+        @ Bx2, By2 : Coordinates of Left Hip
+        
+        Returns:
+        (x, y) tuple or None if there is no intersection """
 
         d = (By2 - By1) * (Ax2 - Ax1) - (Bx2 - Bx1) * (Ay2 - Ay1)
         if d:
@@ -59,35 +59,35 @@ class pose_detection():
         return int(x), int(y)    
     
     def distance_from_center(self, w, pt):
-    """ 
-    Params:
-    @ w : width of the frame
-    @ pt : x value computed from line intersect
-    
-    Returns:
-    Number pixels away from the center of the frame """
+        """ 
+        Params:
+        @ w : width of the frame
+        @ pt : x value computed from line intersect
+        
+        Returns:
+        Number pixels away from the center of the frame """
     
         return (int(w/2)-pt[0])
     
-    def Distance_finder(self, Focal_Length, real_sholuder_width, shoulder_width_in_frame):
-    """ 
-    Params:
-    @ Focal_Length : Focal legth of the camera
-    @ real_sholuder_width : Measured value of the user / person (known width)
-    @ shoulder_width_in_frame : shoulder width computed using eculidean distance
-    
-    Return:
-    The distance of person in accordance with camera """
+    def Distance_finder(self, Focal_Length, real_shoulder_width, shoulder_width_in_frame):
+        """ 
+        Params:
+        @ Focal_Length : Focal legth of the camera
+        @ real_sholuder_width : Measured value of the user / person (known width)
+        @ shoulder_width_in_frame : shoulder width computed using eculidean distance
+        
+        Return:
+        The distance of person in accordance with camera """
     
         return (real_shoulder_width * Focal_Length)/shoulder_width_in_frame
     
     def distances(self, img_from_ros):
-    """ 
-    Params:
-    @ img_from_ros: frame that is being sent from the ros
-    
-    Return:
-    The lateral_deviation(pixels) and depth(cm) of person """
+        """ 
+        Params:
+        @ img_from_ros: frame that is being sent from the ros
+        
+        Return:
+        The lateral_deviation(pixels) and depth(cm) of person """
 
         with self.mp_pose.Pose(min_detection_confidence=0.2,min_tracking_confidence=0.2) as pose:
             
@@ -96,7 +96,6 @@ class pose_detection():
             image = cv2.flip(img_from_ros, 1)
 
             results = pose.process(img_from_ros)
-            x_distance = 0
             pt = (0,0)
             if results.pose_landmarks is not None:
                 RSHcoordinate, LSHcoordinate, RHcoordinate, LHcoordinate = self.coordinates(results,h, w)
@@ -108,8 +107,8 @@ class pose_detection():
                 pt = self.line_intersect(a, b, c, d, e, f, g, h)
                 if pt is not  None:
                     lateral_deviation = self.distance_from_center(w, pt)
-                    
+            else:
+            	lateral_deviation= 0
+            	depth = 0
+ 
         return lateral_deviation, depth
-
-
-		
